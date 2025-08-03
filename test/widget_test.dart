b27012e564f0e06fa091c46234f3e1930e3a1bf7
marc obscherningkat <marc.obscherningkat@gmx.de>
2025-08-04 00:47:35 +0200

    Fix widget_test to provide ThemeController for MyAppWrapper

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,11 +1,18 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:nwe_isee_app/main.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:nwe_isee_app/main.dart'; // enthält MyAppWrapper
+import 'package:nwe_isee_app/theme_controller.dart';
 
 void main() {
   testWidgets('App lädt und zeigt Text', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp(initialMode: ThemeMode.system));
-
+    await tester.pumpWidget(
+      ChangeNotifierProvider(
+        create: (_) => ThemeController(),
+        child: const MyAppWrapper(),
+      ),
+    );
+    await tester.pumpAndSettle();
     expect(find.text('Hallo, Welt!'), findsOneWidget);
   });
 }
